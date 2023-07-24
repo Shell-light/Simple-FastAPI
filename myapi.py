677e88b002c0d759a8de946a7916f9b454fbc4b9
neon1 <ulightshell@gmail.com>
2023-07-24 17:43:20 +0500

    Path Parameters

--- myapi.py
+++ myapi.py
@@ -15,21 +15,5 @@ def index():
     return {"name": "First Data"}
 
 @app.get("/get-student/{student_id}")
-def get_student(student_id: int= Path(None, description="The ID of the student you want to view", gt=0, lt=3)):
+def get_student(student_id: int = Path(description="The ID of the student you want to view", gt=0, lt=3)):
     return students[student_id]
-
-
-
-
-
-
-
-
-
-"""
-endpoint - localhost/delete-user
-GET - GET ANY INFORMATION
-POST - CREATE SOMETHING
-PUT - UPDATE
-DELETE - DELETE SOMTHING
-"""
